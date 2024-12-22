bb58db4ec74c46584f741e2f8bec15055573cd51
Oleksandr231989 <124906757+Oleksandr231989@users.noreply.github.com>
2024-12-22 17:13:30 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    powerBiUrl: "https://app.powerbi.com/view?r=eyJrIjoiNjlkMjBmYjItNDljNy00MDQ0LTgyZWQtMjEyMTQxYWJkYzcxIiwidCI6IjZjZjFjYWUyLTZmNTgtNDA3YS04ZWY4LWNiZDY0MjMyYWIzYSIsImMiOjl9",
+    firebase: {
+        apiKey: "AIzaSyA-wHAQkA9fTM8VzuiqTG6mne7NHQv8cy4",
+            authDomain: "mayoly-28298.firebaseapp.com",
+            projectId: "mayoly-28298",
+            storageBucket: "mayoly-28298.appspot.com",
+            messagingSenderId: "321318380841",
+            appId: "1:321318380841:web:exampleappid"
+    }
+};
